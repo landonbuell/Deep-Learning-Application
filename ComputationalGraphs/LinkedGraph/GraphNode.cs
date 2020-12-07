ff34d5212d7042feedffe437550345b2c2e25493
landonbuell <lhb1007@wildcats.unh.edu>
2020-12-07 11:50:53 -0500

    Updated to Computational Graph

---
+++ ComputationalGraphs/LinkedGraph/GraphNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedGraph
+{
+    public class GraphNode
+    {
+        public string Name { get; protected set; }
+
+        public int Index { get; protected set; }
+
+        protected List<GraphNode> NextNodes { get; set; }
+
+        protected List<GraphNode> PrevNodes { get; set; }
+
+        public GraphNode(string name, int index,
+            List<GraphNode> nextNodes, List<GraphNode> prevNodes)
+        {
+            // Constructor for Graph Node
+            Name = name;
+            Index = index;
+
+            NextNodes = nextNodes;
+            PrevNodes = prevNodes;
+        }
+
+
+    }
+
+}
---
+++ ComputationalGraphs/LinkedGraph/LinkedGraphMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedGraph
+{
+    class LinkedGraphMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
